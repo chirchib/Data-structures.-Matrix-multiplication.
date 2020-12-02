a29d5af65d712109e7d12ed9dee7a5ad5772bd0a
chirchib <dmitry.kurochkin66@gmail.com>
2020-12-03 01:21:50 +0300

    deleted fck winograd

--- Structures_laba_2.cpp
+++ Structures_laba_2.cpp
@@ -8,11 +8,25 @@ float Random(float min, float max)
 	return  (float)(rand()) / RAND_MAX * (max - min) + min;
 }
 
+void DisplayMat(float mat[], int size)
+{
+	cout << "\n\n" ;
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+		{
+			cout << mat[size * i + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "\n\n";
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	const unsigned int N = 2048;
+	const unsigned int N = 1;
 	float min = 0.0, max = 10.0;
 	float *mat_A = new float[N * N];
 	float *mat_B = new float[N * N];
@@ -45,55 +59,33 @@ int main()
 		}
 	}
 	unsigned int end_time1 = clock();
+	DisplayMat(mat_C, N * N);
 	delete[] mat_C;
 	cout << "1-ый вариант перемножения: Формула из линейной алгебры." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
-	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time1 * pow(10, -6);
+	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time1 * pow(10, -6) << endl;
 
 	// func cblas_sgemm from BLAS
 	unsigned int start_time2 = clock();
-	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1, mat_A, N, mat_B, N, 1, mat_D, N);
+	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, mat_A, N, mat_B, N, 0.0, mat_D, N);
 	unsigned int end_time2 = clock();
+
+	DisplayMat(mat_D, N * N);
 	delete[] mat_D;
 	cout << "\n\n2-ой вариант перемножения: Результат работы функции cblas_sgemm \nиз библиотеки BLAS (рекомендуемая реализация из Intel MKL)." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
-	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time2 * pow(10, -6);
+	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time2 * pow(10, -6) << endl;
 
 	// my way
 	// Winograd's algorithm	
 	unsigned int start_time3 = clock();
-	float rowFactor[N];
-	float columnFactor[N];
-	for (int i = 0; i < N; ++i)
-	{
-		rowFactor[i] = mat_A[i * N + 1] * mat_A[i * N + 2];
-		columnFactor[i] = mat_B[N + i] * mat_B[2 * N + i];
-		for (int j = 1; j < N / 2; ++j)
-		{
-			rowFactor[i] += mat_A[i * N + 2 * j - 1] * mat_A[i * N + 2 * j];
-			columnFactor[i] += mat_B[2 * j * N - 1 + i] * mat_B[2 * j * N + i];
-		}
-	}
-
-	for (int i = 0; i < N; ++i)
-	{
-		columnFactor[i] = mat_B[N + i] * mat_B[2 * N + i];
-		for (int j = 1; j < N / 2; ++j)
-			columnFactor[i] += mat_B[2 * j * N - 1 + i] * mat_B[2 * j * N + i];
-	}
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 1; j < N; ++j)
-		{
-			mat_E[i * N + j] = -rowFactor[i] - columnFactor[j];
-			for (int k = 0; k < N / 2; ++k)
-				mat_E[i * N + j] += (mat_A[i * N + 2 * k - 1] + mat_B[2 * k * N + j]) * (mat_A[i * N + 2 * k] + mat_B[2 * k * N + j]);
-		}
-	}
+	
 	unsigned int end_time3 = clock();
+
+
 	cout << "\n\n3-ий вариант перемножения: Оптимизированный алгоритм по моему выбору, \nнаписанный мной, производительность должна быть не ниже 30% от 2-го варианта." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
-	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time3 * pow(10, -6);
+	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time3 * pow(10, -6) << endl;
 
 
 	cout << "\n\n\nФИТУ 2-5, Курочкин Дмитрий Сергеевич";
