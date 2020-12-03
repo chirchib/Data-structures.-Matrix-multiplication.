ea47893d53cf4d7962a9455602fe337d7921f519
chirchib <dmitry.kurochkin66@gmail.com>
2020-12-03 20:55:15 +0300

    Added alg to main

--- Strassen's Algorithm.cpp
+++ Strassen's Algorithm.cpp
@@ -1,130 +1,157 @@
 #include "Strassen's Algorithm.h"
 #include <iostream>
 #include <ctime>
-using namespace std;
 
-float** create_mat(int size_mat)
+float* Strassen(float* matA, float* matB, int size)
 {
-	float** mat = new float* [size_mat];
-	for (int i = 0; i < size_mat; ++i)
+	float** mat1 = create_mat(size);
+	float** mat2 = create_mat(size);
+
+	for (int i = 0; i < size; ++i)
 	{
-		mat[i] = new float[size_mat];
-		for (int j = 0; j < size_mat; ++j)
+		for (int j = 0; j < size; ++j)
+		{
+			mat1[i][j] = matA[i * size + j];
+			mat2[i][j] = matB[i * size + j];
+		}
+	}
+
+	float** mat_res = mult_mat(mat1, mat2, size);
+
+	float* matE = new float[size];
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+		{
+			matE[i * size + j] = mat_res[i][j];
+		}
+	}
+
+	return matE;
+}
+
+float** create_mat(int size)
+{
+	float** mat = new float* [size];
+	for (int i = 0; i < size; ++i)
+	{
+		mat[i] = new float[size];
+		for (int j = 0; j < size; ++j)
 			mat[i][j] = 0;
 	}
 
 	return mat;
 }
 
-float** mult(float** mat1, float** mat2, int size_mat)
+float** mult(float** mat1, float** mat2, int size)
 {
-	float** mat_res = create_mat(size_mat);
-	for (int i = 0; i < size_mat; ++i)
-		for (int j = 0; j < size_mat; ++j)
-			for (int k = 0; k < size_mat; ++k)
+	float** mat_res = create_mat(size);
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
+			for (int k = 0; k < size; ++k)
 				mat_res[i][j] += mat1[i][k] * mat2[k][j];
 
 	return mat_res;
 }
 
-void split_4mat(float** mat, float** mat11, float** mat12, float** mat21, float** mat22, int size_mat)
+void split_4mat(float** mat, float** mat11, float** mat12, float** mat21, float** mat22, int size)
 {
-	int new_size_mat = size_mat / 2;
-	for (int i = 0; i < new_size_mat; ++i)
-		for (int j = 0; j < new_size_mat; ++j)
+	int new_size = size / 2;
+	for (int i = 0; i < new_size; ++i)
+		for (int j = 0; j < new_size; ++j)
 			mat11[i][j] = mat[i][j];
 
-	for (int i = 0; i < new_size_mat; ++i)
-		for (int j = new_size_mat; j < size_mat; ++j)
-			mat12[i][j - new_size_mat] = mat[i][j];
+	for (int i = 0; i < new_size; ++i)
+		for (int j = new_size; j < size; ++j)
+			mat12[i][j - new_size] = mat[i][j];
 
-	for (int i = new_size_mat; i < size_mat; ++i)
-		for (int j = 0; i < new_size_mat; ++j)
-			mat21[i - new_size_mat][j] = mat[i][j];
+	for (int i = new_size; i < size; ++i)
+		for (int j = 0; i < new_size; ++j)
+			mat21[i - new_size][j] = mat[i][j];
 
-	for (int i = new_size_mat; i < size_mat; ++i)
-		for (int j = new_size_mat; i < size_mat; ++j)
-			mat22[i - new_size_mat][j - new_size_mat] = mat[i][j];
+	for (int i = new_size; i < size; ++i)
+		for (int j = new_size; i < size; ++j)
+			mat22[i - new_size][j - new_size] = mat[i][j];
 }
 
-float** collect_4mat(float** mat11, float** mat12, float** mat21, float** mat22, int size_mat)
+float** collect_4mat(float** mat11, float** mat12, float** mat21, float** mat22, int size)
 {
-	int new_size_mat = 2 * size_mat;
-	float** mat = create_mat(new_size_mat);
+	int new_size = 2 * size;
+	float** mat = create_mat(new_size);
 
-	for (int i = 0; i < size_mat; ++i)
-		for (int j = 0; j < size_mat; ++j)
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
 			mat[i][j] = mat11[i][j];
 
-	for (int i = 0; i < size_mat; ++i)
-		for (int j = size_mat; j < new_size_mat; ++j)
-			mat[i][j] = mat12[i][j - size_mat];
+	for (int i = 0; i < size; ++i)
+		for (int j = size; j < new_size; ++j)
+			mat[i][j] = mat12[i][j - size];
 
-	for (int i = size_mat; i < new_size_mat; ++i)
-		for (int j = 0; i < size_mat; ++j)
-			mat[i][j] = mat21[i - size_mat][j];
+	for (int i = size; i < new_size; ++i)
+		for (int j = 0; i < size; ++j)
+			mat[i][j] = mat21[i - size][j];
 
-	for (int i = size_mat; i < new_size_mat; ++i)
-		for (int j = size_mat; i < new_size_mat; ++j)
-			mat[i][j] = mat22[i - size_mat][j - size_mat];
+	for (int i = size; i < new_size; ++i)
+		for (int j = size; i < new_size; ++j)
+			mat[i][j] = mat22[i - size][j - size];
 
 	return mat;
 }
 
-float** sum_mat(float** mat1, float** mat2, int size_mat)
+float** sum_mat(float** mat1, float** mat2, int size)
 {
-	float** mat = create_mat(size_mat);
-	for (int i = 0; i < size_mat; ++i)
-		for (int j = 0; j < size_mat; ++j)
+	float** mat = create_mat(size);
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
 			mat[i][j] = mat1[i][j] + mat2[i][j];
 
 	return mat;
 }
 
-float** diff_mat(float** mat1, float** mat2, int size_mat)
+float** diff_mat(float** mat1, float** mat2, int size)
 {
-	float** mat = create_mat(size_mat);
-	for (int i = 0; i < size_mat; ++i)
-		for (int j = 0; j < size_mat; ++j)
+	float** mat = create_mat(size);
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
 			mat[i][j] = mat1[i][j] - mat2[i][j];
 
 	return mat;
 }
 
-float** mult_mat(float** mat1, float** mat2, int size_mat)
+float** mult_mat(float** mat1, float** mat2, int size)
 {
-	if (size_mat <= 64)
-		return mult(mat1, mat2, size_mat);
+	if (size <= 64)
+		return mult(mat1, mat2, size);
 	else
 	{
-		size_mat /= 2;
+		size /= 2;
 
-		float** a11 = create_mat(size_mat);
-		float** a12 = create_mat(size_mat);
-		float** a21 = create_mat(size_mat);
-		float** a22 = create_mat(size_mat);
+		float** a11 = create_mat(size);
+		float** a12 = create_mat(size);
+		float** a21 = create_mat(size);
+		float** a22 = create_mat(size);
 		
-		float** b11 = create_mat(size_mat);
-		float** b12 = create_mat(size_mat);
-		float** b21 = create_mat(size_mat);
-		float** b22 = create_mat(size_mat);
+		float** b11 = create_mat(size);
+		float** b12 = create_mat(size);
+		float** b21 = create_mat(size);
+		float** b22 = create_mat(size);
 
-		split_4mat(mat1, a11, a12, a21, a22, size_mat * 2);
-		split_4mat(mat2, b11, b12, b21, b22, size_mat * 2);
+		split_4mat(mat1, a11, a12, a21, a22, size * 2);
+		split_4mat(mat2, b11, b12, b21, b22, size * 2);
 
-		float** p1 = mult_mat(sum_mat(a11, a22, size_mat), sum_mat(b11, b22, size_mat), size_mat);
-		float** p2 = mult_mat(sum_mat(a21, a22, size_mat), b11, size_mat);
-		float** p3 = mult_mat(a11, diff_mat(b12, b22, size_mat), size_mat);
-		float** p4 = mult_mat(a22, diff_mat(b21, b11, size_mat), size_mat);
-		float** p5 = mult_mat(sum_mat(a11, a12, size_mat), b22, size_mat);
-		float** p6 = mult_mat(sum_mat(a21, a11, size_mat), sum_mat(b11, b12, size_mat), size_mat);
-		float** p7 = mult_mat(sum_mat(a12, a22, size_mat), sum_mat(b21, b22, size_mat), size_mat);
+		float** p1 = mult_mat(sum_mat(a11, a22, size), sum_mat(b11, b22, size), size);
+		float** p2 = mult_mat(sum_mat(a21, a22, size), b11, size);
+		float** p3 = mult_mat(a11, diff_mat(b12, b22, size), size);
+		float** p4 = mult_mat(a22, diff_mat(b21, b11, size), size);
+		float** p5 = mult_mat(sum_mat(a11, a12, size), b22, size);
+		float** p6 = mult_mat(sum_mat(a21, a11, size), sum_mat(b11, b12, size), size);
+		float** p7 = mult_mat(sum_mat(a12, a22, size), sum_mat(b21, b22, size), size);
 		
-		float** c11 = sum_mat(diff_mat(p1, p5, size_mat), sum_mat(p4, p7, size_mat), size_mat);
-		float** c12 = sum_mat(p3, p5, size_mat);
-		float** c21 = sum_mat(p2, p4, size_mat);
-		float** c22 = sum_mat(diff_mat(p1, p2, size_mat), sum_mat(p3, p6, size_mat), size_mat);
+		float** c11 = sum_mat(diff_mat(p1, p5, size), sum_mat(p4, p7, size), size);
+		float** c12 = sum_mat(p3, p5, size);
+		float** c21 = sum_mat(p2, p4, size);
+		float** c22 = sum_mat(diff_mat(p1, p2, size), sum_mat(p3, p6, size), size);
 
-		return collect_4mat(c11, c12, c21, c22, size_mat);
+		return collect_4mat(c11, c12, c21, c22, size);
 	}
 }
--- Structures_laba_2.cpp
+++ Structures_laba_2.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <mkl_cblas.h>
+#include "Strassen's Algorithm.h"
 using namespace std;
 
 float Random(float min, float max)
@@ -8,7 +9,7 @@ float Random(float min, float max)
 	return  (float)(rand()) / RAND_MAX * (max - min) + min;
 }
 
-void DisplayMat(float mat[], int size)
+void DisplayMat(float* mat, int size)
 {
 	cout << "\n\n" ;
 	for (int i = 0; i < size; ++i)
@@ -26,7 +27,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	const unsigned int N = 1;
+	const unsigned int N = 64;
 	float min = 0.0, max = 10.0;
 	float *mat_A = new float[N * N];
 	float *mat_B = new float[N * N];
@@ -48,6 +49,7 @@ int main()
 	}
 	
 	// linear algebra
+
 	unsigned int start_time1 = clock();
 	for (int i = 0; i < N; ++i)
 	{
@@ -59,19 +61,19 @@ int main()
 		}
 	}
 	unsigned int end_time1 = clock();
-	DisplayMat(mat_C, N * N);
 	delete[] mat_C;
+
 	cout << "1-ый вариант перемножения: Формула из линейной алгебры." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
 	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time1 * pow(10, -6) << endl;
 
 	// func cblas_sgemm from BLAS
+
 	unsigned int start_time2 = clock();
 	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, mat_A, N, mat_B, N, 0.0, mat_D, N);
 	unsigned int end_time2 = clock();
-
-	DisplayMat(mat_D, N * N);
 	delete[] mat_D;
+
 	cout << "\n\n2-ой вариант перемножения: Результат работы функции cblas_sgemm \nиз библиотеки BLAS (рекомендуемая реализация из Intel MKL)." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
 	cout << "Производительность в MFlops: " << 2 * pow(N, 3) / end_time2 * pow(10, -6) << endl;
@@ -80,9 +82,9 @@ int main()
 	// Strassen's algorithm
 
 	unsigned int start_time3 = clock();
-	
+	mat_E  = Strassen(mat_A, mat_B, N);
 	unsigned int end_time3 = clock();
-
+	delete[] mat_E;
 
 	cout << "\n\n3-ий вариант перемножения: Оптимизированный алгоритм по моему выбору, \nнаписанный мной, производительность должна быть не ниже 30% от 2-го варианта." << endl;
 	cout << "Сложность алгоритма: " << 2 * pow(N, 3) << endl;
