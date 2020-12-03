3198eaed7138e00de777d44fc712713bfd03f0ee
chirchib <dmitry.kurochkin66@gmail.com>
2020-12-03 20:55:26 +0300

    Revert "Added alg to main"
    This reverts commit 244073fa9d400138e530665f49917f36355c3407.

--- Strassen's Algorithm.h
+++ Strassen's Algorithm.h
@@ -1,16 +1,19 @@
-float* Strassen(float*, float*, int);
+class Strassen
+{
+public:
+	float** mult(float**, float**, int);
 
-float** mult(float**, float**, int);
+	float** mult_mat(float**, float**, int);
+private:
+	float** create_mat(int);
 
-float** mult_mat(float**, float**, int);
+	float** create_mat_rand(int);
+	
+	void split_4mat(float**, float**, float*, float**, float**, int);
 
-float** create_mat(int);
+	float** collect_4mat(float**, float**, float**, float**, int);
 
-void split_mat(float**, float**, float**, float**, float**, int);
+	float** sum_mat(float**, float**, int);
 
-float** collect_4mat(float**, float**, float**, float**, int);
-
-float** sum_mat(float**, float**, int);
-
-float** diff_mat(float**, float**, int);
-
+	float** diff_mat(float**, float**, int);
+};
